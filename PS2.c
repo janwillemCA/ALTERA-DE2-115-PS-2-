f6bdf1a4e27f121d339bb34f00aa09e587736835
Jan Willem Casteleijn <jw.ati44@gmail.com>
2017-05-22 18:07:40 +0200

    Update PS2.c
    added main and cos

--- PS2.c
+++ PS2.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include "includes.h"
+#include <altera_up_avalon_ps2.h>
+#include <altera_up_ps2_keyboard.h>
+
+/* Definition of Task Stacks */
+#define         TASK_STACKSIZE                          2048
+OS_STK taskKeyboard_stk                                [TASK_STACKSIZE];
+
+#define         taskKeyboard_PRIORITY                   1
+
 void taskKeyboard(void* pdata)
 {
   INT8U err;
@@ -22,4 +33,13 @@ void taskKeyboard(void* pdata)
           OSTimeDlyHMSM(0, 0, 0, 100);
         }
     }
+}
+
+/* The main function creates two task and starts multi-tasking */
+int main(void)
+{
+  OSTaskCreateExt(taskKeyboard,         NULL,   (void *)&taskKeyboard_stk[TASK_STACKSIZE-1],            taskKeyboard_PRIORITY,          taskKeyboard_PRIORITY,          taskKeyboard_stk,       TASK_STACKSIZE, NULL,   0);
+
+  OSStart();
+  return 0;
 }
